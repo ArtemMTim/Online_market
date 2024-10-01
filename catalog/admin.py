50bf8536b9a1efd03df46dd1ae82ba486fb6bf25
Timofeev Artem <artemtimofeev@rambler.ru>
2024-10-01 07:05:14 +0300

    Создание моделей, настройка админки, фикстуры.

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "title")
+    search_fields = ("title",)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "title",
+        "category",
+        "price",
+    )
+    list_filter = ("category",)
+    search_fields = ("title", "description")
+
 
 # Register your models here.
--- catalog/models.py
+++ catalog/models.py
@@ -2,8 +2,15 @@ from django.db import models
 
 
 class Category(models.Model):
-    title = models.CharField(max_length=150, verbose_name="Название")
-    description = models.TextField(verbose_name="Описание")
+    title = models.CharField(
+        max_length=150, verbose_name="Название", help_text="Введите название категории"
+    )
+    description = models.TextField(
+        verbose_name="Описание",
+        blank=True,
+        null=True,
+        help_text="Введите описание категории",
+    )
 
     def __str__(self):
         return self.title
@@ -15,16 +22,29 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    title = models.CharField(max_length=150, verbose_name="Название")
-    description = models.TextField(verbose_name="Описание")
-    image = models.ImageField(upload_to="images/", verbose_name="Изображение")
+    title = models.CharField(
+        max_length=150, verbose_name="Название", help_text="Введите название продукта"
+    )
+    description = models.TextField(
+        verbose_name="Описание",
+        blank=True,
+        null=True,
+        help_text="Введите описание продукта",
+    )
+    image = models.ImageField(
+        upload_to="images/",
+        verbose_name="Изображение",
+        blank=True,
+        null=True,
+        help_text="Загрузите фотографию продукта",
+    )
     category = models.ForeignKey(
         Category,
         on_delete=models.CASCADE,
         related_name="products",
         verbose_name="Категория",
     )
-    price = models.FloatField(verbose_name="Цена")
+    price = models.FloatField(verbose_name="Цена", help_text="Введите цену продукта")
     created_at = models.DateField(auto_now_add=True, verbose_name="Дата создания")
     updated_at = models.DateField(auto_now=True, verbose_name="Дата изменения")
 
