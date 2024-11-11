c97ea74a745f0edf796018388a05be187049d84c
ArtemMTim <artemtimofeev@rambler.ru>
2024-11-11 20:49:57 +0300

    Кэширование и сервисный слой.

--- catalog/views.py
+++ catalog/views.py
@@ -6,11 +6,24 @@ from django.views.generic import DetailView, ListView, TemplateView, View
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
 from .forms import ProductForm
-from .models import Product
+from .models import Category, Product
+from .service import category_products, get_product_list
+
+
+class CategoryListView(ListView):
+    """Контроллер отображения списка категорий продуктов."""
+
+    model = Category
+    template_name = "catalog/category_list.html"
 
 
 class ProductListView(ListView):
+    """Контроллер отображения списка всех опубликованных продуктов."""
+
     model = Product
+
+    def get_queryset(self):
+        return get_product_list()
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -19,10 +32,14 @@ class ProductListView(ListView):
 
 
 class ContactsView(TemplateView):
+    """Контроллер отображения страницы контактов."""
+
     template_name = "catalog/contacts.html"
 
 
 class ProductDetailView(LoginRequiredMixin, DetailView):
+    """Контроллер отображения деталей о продукте."""
+
     model = Product
     login_url = reverse_lazy("users:login")
 
@@ -34,6 +51,8 @@ class ProductDetailView(LoginRequiredMixin, DetailView):
 
 
 class ProductCreateView(LoginRequiredMixin, CreateView):
+    """Контроллер создания продукта."""
+
     model = Product
     form_class = ProductForm
     template_name = "catalog/product_form.html"
@@ -49,6 +68,8 @@ class ProductCreateView(LoginRequiredMixin, CreateView):
 
 
 class ProductDeleteView(LoginRequiredMixin, DeleteView):
+    """Контроллер удаления продукта."""
+
     def post(self, request, pk):
         product = get_object_or_404(Product, pk=pk)
         if (
@@ -63,6 +84,8 @@ class ProductDeleteView(LoginRequiredMixin, DeleteView):
 
 
 class ProductUpdateView(LoginRequiredMixin, UpdateView):
+    """Контроллер изменения продукта."""
+
     def post(self, request, pk):
         product = get_object_or_404(Product, pk=pk)
         if self.request.user != product.owner:
@@ -79,6 +102,8 @@ class ProductUpdateView(LoginRequiredMixin, UpdateView):
 
 
 class UnpublishProductView(LoginRequiredMixin, View):
+    """Контроллер снятия продукта с публикации на главной странице"""
+
     def post(self, request, pk):
         product = get_object_or_404(Product, pk=pk)
         if not request.user.has_perm("can_unpublish_product"):
@@ -87,6 +112,25 @@ class UnpublishProductView(LoginRequiredMixin, View):
         product.publish_status = False
         product.save()
         return redirect("catalog:product_list")
+
+
+class CategoryProductView(LoginRequiredMixin, ListView):
+    """Контроллер отображения всех продуктов в отдельной категории."""
+
+    template_name = "catalog/category_product.html"
+    context_object_name = "products"
+    login_url = reverse_lazy("users:login")
+
+    def get_queryset(self):
+        print(self.kwargs)
+        pk = self.kwargs.get("pk")
+        print(pk)
+        return category_products(pk)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["products"] = self.get_queryset()
+        return context
 
 
 # Create your views here.
